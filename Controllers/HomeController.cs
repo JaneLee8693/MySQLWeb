469ea6b6fd562633583972c7835052d2825f62a8
Jane Lee <xo930497@yahoo.com.tw>
2022-03-29 20:46:59 -0600

    Add project files.

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Logging;
+using MySQLWeb.Models;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MySQLWeb.Controllers
+{
+    public class HomeController : Controller
+    {
+        private IBowlersRepository _repo { get; set; }
+
+        public HomeController(IBowlersRepository temp)
+        {
+            _repo = temp;
+        }
+
+        public IActionResult Index()
+        {
+            var blah = _repo.Bowlers
+                .OrderBy(b => b.BowlerFirstName);
+                //.ToList();
+
+            return View(blah);
+        }
+
+    }
+}
---
+++ Models/Bowler.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MySQLWeb.Models
+{
+    public class Bowler
+    {
+        [Key]
+        [Required]
+        public int BowlerID { get; set; }
+
+        [Required(ErrorMessage = "Pleasee enter a valid last name")]
+        public string BowlerLastName { get; set; }
+
+        [Required(ErrorMessage = "Pleasee enter a valid first name")]
+        public string BowlerFirstName { get; set; }
+
+        [MaxLength(1)]
+        public string BowlerMiddleInit { get; set; }
+        public string BowlerAddress { get; set; }
+        public string BowlerCity { get; set; }
+
+        [MaxLength(2)]
+        public string BowlerState { get; set; }
+
+        [MaxLength(10)]
+        public string BowlerZip { get; set; }
+
+        [MaxLength(14)]
+        public string BowlerPhoneNumber { get; set; }
+
+        //build the foreign key relationship
+        [Required(ErrorMessage = "Pleasee enter a valid team")]
+        public int TeamId { get; set; }
+        public Team TeamName { get; set; }
+    }
+}
